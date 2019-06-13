b60c070b9683d279c88e513da758a471f6e2b535
Michael Eselgroth <michael.eselgroth@just-eat.com>
2019-06-13 21:32:14 +0100

    Added virtual card and withdraw method with tests

--- CreditSuisse/VirtualCard.cs
+++ CreditSuisse/VirtualCard.cs
@@ -2,5 +2,38 @@
 {
     public class VirtualCard
     {
+        private readonly object _transactionLock = new object();
+        private readonly short _pin;
+        public VirtualCard(short pin, decimal startingBalance)
+        {
+            _pin = pin;
+            Balance = startingBalance;
+        }
+
+        public decimal Balance { get; private set; }
+
+        public WithdrawResult Withdraw(short pin, decimal amount)
+        {
+            if (pin != _pin)
+            {
+                return WithdrawResult.InvalidPin;
+            }
+            lock (_transactionLock)
+            {
+                if (Balance < amount)
+                {   
+                    return WithdrawResult.LowBalance;
+                }
+                Balance -= amount;
+            }
+            return WithdrawResult.Ok;
+        }
+    }
+
+    public enum WithdrawResult
+    {
+        Ok,
+        InvalidPin,
+        LowBalance
     }
 }
--- CreditSuisseTests/VirtualCardTest.cs
+++ CreditSuisseTests/VirtualCardTest.cs
@@ -1,14 +1,42 @@
+using CreditSuisse;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace CreditSuisseTests
 {
     public class VirtualCardTest
     {
+        [Test]
+        public void WithdrawShouldReturnOkAndBalanceUpdated()
+        {
+            const short pin = 0123;
+            var sut = new VirtualCard(pin, 30);
+
+            var result = sut.Withdraw(pin, 10);
+
+            result.Should().Be(WithdrawResult.Ok);
+            sut.Balance.Should().Be(20);
+        }
 
         [Test]
-        public void Test1()
+        public void WithdrawWithBadPinShouldReturnBadPin()
         {
-            
+            var sut = new VirtualCard(0123, 30);
+
+            var result = sut.Withdraw(9999, 10);
+
+            result.Should().Be(WithdrawResult.InvalidPin);
+        }
+
+        [Test]
+        public void WithdrawWithLowBalanceShouldReturnBadPin()
+        {
+            const short pin = 0123;
+            var sut = new VirtualCard(pin, 10);
+
+            var result = sut.Withdraw(pin, 20);
+
+            result.Should().Be(WithdrawResult.LowBalance);
         }
     }
 }
