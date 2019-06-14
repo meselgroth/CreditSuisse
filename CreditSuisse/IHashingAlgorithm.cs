026e11e58db2f407b4237fe147a7c0a7e01d4a4d
Michael Eselgroth <michael.eselgroth@just-eat.com>
2019-06-14 09:23:32 +0100

    Refactor

--- CreditSuisse/IHashingAlgorithm.cs
+++ CreditSuisse/IHashingAlgorithm.cs
@@ -1,29 +1,7 @@
-using System.Security.Cryptography;
-using System.Text;
-
 namespace CreditSuisse
 {
     public interface IHashingAlgorithm
     {
         string Hash(string pin);
-    }
-
-    public class Sha256HashingAlgorithm : IHashingAlgorithm
-    {
-        public string Hash(string pin)
-        {
-            using (var sha256Hash = SHA256.Create())
-            {
-                var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(pin));
-
-                // Convert byte array to a string   
-                var builder = new StringBuilder();
-                foreach (var b in bytes)
-                {
-                    builder.Append(b.ToString("x2"));
-                }
-                return builder.ToString();
-            }
-        }
     }
 }
